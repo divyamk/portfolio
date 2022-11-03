7ab072602aff44248c6b06a08c0e80739084a477
divyamk <divyam.khatri@yahoo.com>
2022-11-03 10:38:10 -0500

    Added about information

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import './index.css';;
-  
+import './index.css'
+
 const About = () => {
   return (
     <>
       <div className="element" id='about'>
         <h1>About</h1>
+        <div id="about-info">
+          <p>Hi, I am a Computer Science and Cognitive Science double major at the University of Texas at Dallas.
+            I love Aritificial Intelligence, Machine Learning, UX Design, and the human brain.
+          </p>
+        </div>
+
       </div>
     </>
   );
 };
-  
+
 export default About;
